59fc605a4d7a719c22ef603216139dfb332bafb5
kasonyang <i@kasonyang.com>
2014-08-03 21:08:16 +0800

    add class:validator

--- libs/Validator.php
+++ libs/Validator.php
@@ -10,3 +10,17 @@ namespace Hiano\Validator;
 interface ValidatorInterface{
     function validate($value);
 }
+
+class Validator{
+    /**
+     * 
+     * @param mixed $value
+     * @param string $validator_name
+     * @return boolean
+     */
+    static function validate($value,$validator_name){
+        $validator = new $validator_name();
+        /* @var $validator ValidatorInterface */
+        return $validator->validate($value);
+    }
+}
