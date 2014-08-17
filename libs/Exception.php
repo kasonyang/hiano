0e40c3f2030b6b5bfe6216537affce0a59679fc9
kasonyang <i@kasonyang.com>
2014-08-17 21:27:57 +0800

    add RedirectException,ErrorException

--- libs/Exception.php
+++ libs/Exception.php
@@ -11,8 +11,30 @@ class ValidateException extends \Exception{
     
 }
 
+class ErrorException extends \Exception{
+    
+}
+
+class RedirectException extends \Exception {
+    
+    private $redirect_url;
+    
+    public function __construct($redirect_url,$message = null, $code = null, $previous=null) {
+        parent::__construct($message, $code, $previous);
+        $this->redirect_url = $redirect_url;
+    }
+    
+    function getRedirectUrl(){
+        return $this->redirect_url;
+    }
+}
+
 class Exception extends \Exception{
     static function validateFailed($field,$value,$validator_name){
         throw new ValidateException("Failed to validate the value of {$field}:{$value},'{$validator_name}' required");
+    }
+    
+    static function error($error_description,$error_code){
+        throw new ErrorException($error_description,$error_code);
     }
 }
